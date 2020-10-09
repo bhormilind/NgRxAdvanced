f41e7cad282d5fc3c58401454ec8ecdfe5843f3c
bhormilind <bhormilind@gmail.com>
2020-10-09 11:26:53 -0400

    Added CustomerList component

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -5,10 +5,11 @@ import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomerComponent } from './customer/customer.component';
 import { CustomerAddComponent } from './customer-add/customer-add.component';
 import { CustomerEditComponent } from './customer-edit/customer-edit.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
 
 
 @NgModule({
-  declarations: [CustomerComponent, CustomerAddComponent, CustomerEditComponent],
+  declarations: [CustomerComponent, CustomerAddComponent, CustomerEditComponent, CustomerListComponent],
   imports: [
     CommonModule,
     CustomersRoutingModule
